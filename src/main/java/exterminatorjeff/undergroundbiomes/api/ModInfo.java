e9b9a9bb4e2c42da2376192671c4d781f1d21ec0
Aang23 <qwerty15@gmx.fr>
2018-11-15 19:26:58 +0100

    Bump to 1.3.1

--- src/main/java/exterminatorjeff/undergroundbiomes/api/ModInfo.java
+++ src/main/java/exterminatorjeff/undergroundbiomes/api/ModInfo.java
@@ -9,7 +9,7 @@ public final class ModInfo {
 
   public static final String MODID = "undergroundbiomes";
   public static final String NAME = "Underground Biomes";
-  public static final String VERSION = "1.3.0";
+  public static final String VERSION = "1.3.1";
 
   private ModInfo() {
   }
