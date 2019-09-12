426acbd5b559bd27dfd2ca36cc31b7466d311cc8
Aang23 <qwerty15@gmx.fr>
2019-09-12 21:32:48 +0200

    Fixed sandstone drops. Fixes https://github.com/Aang23/UndergroundBiomesConstructs/issues/162

--- src/main/java/exterminatorjeff/undergroundbiomes/common/block/MetamorphicSandstoneSmooth.java
+++ src/main/java/exterminatorjeff/undergroundbiomes/common/block/MetamorphicSandstoneSmooth.java
@@ -1,9 +1,16 @@
 package exterminatorjeff.undergroundbiomes.common.block;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import com.google.common.base.Predicate;
 import exterminatorjeff.undergroundbiomes.api.enums.UBStoneStyle;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 
@@ -28,4 +35,25 @@ public class MetamorphicSandstoneSmooth extends MetamorphicStone {
     return false;
   }
 
+  @Override
+  public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+    return itemBlock;
+  }
+
+  @Override
+  public List<ItemStack> getDrops(IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+    int meta = state.getBlock().getMetaFromState(state);
+    ItemStack itemStack = new ItemStack(itemBlock, 1, meta);
+    List<ItemStack> result = new ArrayList<ItemStack>();
+    result.add(itemStack);
+    return result;
+  }
+
+  @Override
+  public void getDrops(NonNullList<ItemStack> stacks, IBlockAccess world, BlockPos pos, IBlockState state,
+      int fortune) {
+    int meta = state.getBlock().getMetaFromState(state);
+    ItemStack itemStack = new ItemStack(itemBlock, 1, meta);
+    stacks.add(itemStack);
+  }
 }
