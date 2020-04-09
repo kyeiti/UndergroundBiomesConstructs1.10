5dbc39b3234138a703fea58723174df2544c165f
Aang23 <qwerty15@gmx.fr>
2020-04-09 19:38:07 +0200

    Fix slab drops

--- src/main/java/exterminatorjeff/undergroundbiomes/common/block/slab/UBMetamorphicBrickSlabHalf.java
+++ src/main/java/exterminatorjeff/undergroundbiomes/common/block/slab/UBMetamorphicBrickSlabHalf.java
@@ -1,5 +1,17 @@
 package exterminatorjeff.undergroundbiomes.common.block.slab;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import exterminatorjeff.undergroundbiomes.api.API;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+
 /**
  * @author CurtisA, LouisDB
  */
@@ -10,4 +22,25 @@ public class UBMetamorphicBrickSlabHalf extends UBMetamorphicBrickSlab {
     return false;
   }
 
+  @Override
+  public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+    return API.IGNEOUS_BRICK_SLAB.getItem();
+  }
+
+  @Override
+  public List<ItemStack> getDrops(IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+    int meta = state.getBlock().getMetaFromState(state);
+    ItemStack itemStack = new ItemStack(API.IGNEOUS_BRICK_SLAB.getItem(), 1, meta >= 8 ? meta - 8 : meta);
+    List<ItemStack> result = new ArrayList<ItemStack>();
+    result.add(itemStack);
+    return result;
+  }
+
+  @Override
+  public void getDrops(NonNullList<ItemStack> stacks, IBlockAccess world, BlockPos pos, IBlockState state,
+      int fortune) {
+    int meta = state.getBlock().getMetaFromState(state);
+    ItemStack itemStack = new ItemStack(API.IGNEOUS_BRICK_SLAB.getItem(), 1, meta >= 8 ? meta - 8 : meta);
+    stacks.add(itemStack);
+  }
 }
