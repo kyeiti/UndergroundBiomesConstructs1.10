809cf9e902d47741a65ca26e9395dd9dfccb77ed
Aang23 <qwerty15@gmx.fr>
2020-04-09 14:46:13 +0200

    Drop fixes

--- src/main/java/exterminatorjeff/undergroundbiomes/common/block/SedimentaryMossyCobble.java
+++ src/main/java/exterminatorjeff/undergroundbiomes/common/block/SedimentaryMossyCobble.java
@@ -1,13 +1,21 @@
 package exterminatorjeff.undergroundbiomes.common.block;
 
 import com.google.common.base.Predicate;
+
+import exterminatorjeff.undergroundbiomes.api.API;
 import exterminatorjeff.undergroundbiomes.api.enums.UBStoneStyle;
+import exterminatorjeff.undergroundbiomes.intermod.DropsRegistry;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.NonNullList;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 /**
  * @author CurtisA, LouisDB
@@ -34,6 +42,26 @@ public class SedimentaryMossyCobble extends SedimentaryStone {
   @Override
   public Item getItemDropped(IBlockState state, Random rand, int fortune) {
     return itemBlock;
+  }
+
+  @Override
+  public List<ItemStack> getDrops(IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+    Item mossyCobbleBlock = API.SEDIMENTARY_MOSSY_COBBLE.getItemBlock();
+    int meta = state.getBlock().getMetaFromState(state);
+    ItemStack itemStack = new ItemStack(mossyCobbleBlock , 1, meta);
+    List<ItemStack> result = new ArrayList<ItemStack>();
+    result.add(itemStack);
+    DropsRegistry.INSTANCE.addDrops(result, this, world, pos, state, fortune);
+    return result;
+  }
+
+  @Override
+  public void getDrops(NonNullList<ItemStack> stacks, IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+    Item mossyCobbleBlock  = API.SEDIMENTARY_MOSSY_COBBLE.getItemBlock();
+    int meta = state.getBlock().getMetaFromState(state);
+    ItemStack itemStack = new ItemStack(mossyCobbleBlock , 1, meta);
+    stacks.add(itemStack);
+    DropsRegistry.INSTANCE.addDrops(stacks, this, world, pos, state, fortune);
   }
 
   @Override
